61b8740c17f5b6db46e4acd3f06eb19e3144f990
tavice <thomasavice.ta@gmail.com>
2023-06-17 21:57:15 -0700

    working on touchables

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,23 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, 
+  Text, 
+  View, 
+  Image, 
+  SafeAreaView, 
+  TouchableWithoutFeedback,
+   TouchableOpacity, 
+   TouchableHighlight,
+  TouchableNativeFeedback } from "react-native";
 
 export default function App() {
-  console.log('App executed');
-  let x = 1;
-
+  console.log(require("./assets/icon.png"));
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello React Native</Text>
+      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
+      <View style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }} />
+      </TouchableNativeFeedback>
+      
+      <Image
+        blurRadius={10}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
       <StatusBar style="ativeauto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

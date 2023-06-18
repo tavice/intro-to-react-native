a807dbf64685c6bdeb8fec52c3ccffe5e36c2a15
tavice <thomasavice.ta@gmail.com>
2023-06-17 22:01:47 -0700

    working on buttons

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,32 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, 
-  Text, 
-  View, 
-  Image, 
-  SafeAreaView, 
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
   TouchableWithoutFeedback,
-   TouchableOpacity, 
-   TouchableHighlight,
-  TouchableNativeFeedback } from "react-native";
+  TouchableOpacity,
+  TouchableHighlight,
+  Touch,
+  Button,
+} from "react-native";
 
 export default function App() {
   console.log(require("./assets/icon.png"));
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native</Text>
-      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
-      <View style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }} />
-      </TouchableNativeFeedback>
-      
-      <Image
-        blurRadius={10}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
-      <StatusBar style="ativeauto" />
+      <Button 
+      color="orange"
+      title="Click ME" 
+      onPress={() => console.log("button tapped")} />
     </SafeAreaView>
   );
 }
